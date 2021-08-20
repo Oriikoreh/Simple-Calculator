70255efca57455b5b260fcd6937d0c6380707d88
Guilherme Monteiro <monteiro.oliveira1@outlook.com>
2021-08-20 14:22:24 -0300

    Create operations.c and .h.

---
+++ src/include/operations.h
@@ -0,0 +1,9 @@
+#ifndef SIMPLE_CALCULATOR_OPERATIONS_H
+#define SIMPLE_CALCULATOR_OPERATIONS_H
+
+float plus(float n1, float n2);
+float minus(float n1, float n2);
+float division(float n1, float n2);
+float multiplication(float n1, float n2);
+
+#endif //SIMPLE_CALCULATOR_OPERATIONS_H
