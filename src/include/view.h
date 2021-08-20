c19937c7b15b7cfb15829879e4c921c765edc845
Guilherme Monteiro <monteiro.oliveira1@outlook.com>
2021-08-20 14:23:07 -0300

    Create view.c and .h.

---
+++ src/include/view.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLE_CALCULATOR_VIEW_H
+#define SIMPLE_CALCULATOR_VIEW_H
+
+void startMessage();
+void printAdd(float n1, float n2, float result);
+void printMinus(float n1, float n2, float result);
+void printDivision(float n1, float n2, float result);
+void printMultiplication(float n1, float n2, float result);
+void invalidOperation();
+void wannaTryAgain();
+
+#endif //SIMPLE_CALCULATOR_VIEW_H
