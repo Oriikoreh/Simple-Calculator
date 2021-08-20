db5a6e8f7aef2cf116d5dfeb4833889376081886
Guilherme Monteiro <monteiro.oliveira1@outlook.com>
2021-08-20 14:21:59 -0300

    Create controller.c and .h.

---
+++ src/controller.c
@@ -0,0 +1,51 @@
+#include "include/controller.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+
+void calculatorMenu(){
+    float n1, n2, result;
+    char op;
+    bool exit = false;
+
+    do{
+
+        startMessage();
+        scanf("%f %s %f", &n1, &op, &n2);
+
+        switch (op){
+            case '+':
+                result = plus(n1, n2);
+                printAdd(n1, n2, result);
+                break;
+
+                case '-':
+                    result = minus(n1 ,n2);
+                    printMinus(n1, n2, result);
+                    break;
+
+                    case '/':
+                        result = division(n1, n2);
+                        printDivision(n1, n2, result);
+                        break;
+
+                        case '*':
+                            result = multiplication(n1, n2);
+                            printMultiplication(n1, n2, result);
+                            break;
+
+                            default: invalidOperation();
+                            break;
+        }
+
+        wannaTryAgain();
+        int i;
+        scanf("%d", &i);
+
+        printf("\n\n");
+
+        if(i != 1){
+            exit = true;
+        }
+    }while(!exit);
+}
---
+++ src/include/controller.h
@@ -0,0 +1,9 @@
+#ifndef SIMPLE_CALCULATOR_CONTROLLER_H
+#define SIMPLE_CALCULATOR_CONTROLLER_H
+
+#include "include/operations.h"
+#include "include/view.h"
+
+void calculatorMenu();
+
+#endif //SIMPLE_CALCULATOR_CONTROLLER_H
