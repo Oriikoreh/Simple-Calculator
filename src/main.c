ab84caaed04a82983a649f20e8df128dc3484587
Guilherme Monteiro <monteiro.oliveira1@outlook.com>
2021-08-20 13:04:28 -0300

    Creating a loop for the calculator.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /*
 
@@ -16,34 +17,49 @@ int main(){
 
     float n1, n2, result;
     char op;
+    bool exit = false;
 
-    printf("Please enter with two numbers, and select one operator: +, -, /, *\n");
-    scanf("%f %s %f", &n1, &op, &n2);
+    do{
 
-    switch (op){
-    case '+':
-        result = plus(n1, n2);
-        printf("%.2f plus %.2f equals %.2f", n1, n2, result);
-        break;
+        printf("Please enter with two numbers, and select one operator: +, -, /, *\n");
+        scanf("%f %s %f", &n1, &op, &n2);
 
-    case '-':
-        result = minus(n1 ,n2);
-        printf("%.2f minus %.2f equals %.2f", n1, n2, result);
-        break;
+        switch (op){
+            case '+':
+                result = plus(n1, n2);
+                printf("%.2f plus %.2f equals %.2f", n1, n2, result);
+                break;
+
+                case '-':
+                    result = minus(n1 ,n2);
+                    printf("%.2f minus %.2f equals %.2f", n1, n2, result);
+                    break;
+
+                    case '/':
+                        result = division(n1, n2);
+                        printf("%.2f divided by %.2f equals %.2f", n1, n2, result);
+                        break;
     
-    case '/':
-        result = division(n1, n2);
-        printf("%.2f divided by %.2f equals %.2f", n1, n2, result);
-        break;
+                        case '*':
+                            result = multiplication(n1, n2);
+                            printf("%.2f times %.2f equals %.2f", n1, n2, result);
+                            break;
     
-    case '*':
-        result = multiplication(n1, n2);
-        printf("%.2f times %.2f equals %.2f", n1, n2, result);
-        break;
-    
-    default: printf("Please insert valible numbers or valible operator");
-        break;
-    }
+                            default: printf("Please insert valible numbers or valible operator");
+                            break;
+        }
+
+        printf("\n\nDo you want to leave (0) or carry out another cooperation (1): ");
+        int i;
+        scanf("%d", &i);
+
+        printf("\n\n");
+
+        if(i != 1){
+            exit = true;
+        }
+
+    }while(!exit);
     return 0;
 }
 
